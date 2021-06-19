30e72a58d7e86b462229b041e90959a1c11c3099
Sahin Deniz <seahindeniz@gmail.com>
2021-06-19 17:36:39 +0300

    Use `jsdom`

--- apps/frontend/jest.config.ts
+++ apps/frontend/jest.config.ts
@@ -3,13 +3,13 @@ import type { Config } from '@jest/types';
 const config: Config.InitialOptions = {
   verbose: true,
   collectCoverage: true,
-  roots: [/* './test', */ './src'],
+  roots: ['./src'],
+  testEnvironment: 'jsdom',
   transform: { '^.+\\.tsx?$': 'ts-jest' },
   // https://regex101.com/r/tiSozd/1
   testRegex: '\\.(test|spec)\\.tsx?$',
-  testEnvironment: 'jsdom',
   // testMatch: [
-  //   '**/carsGQL.spec.ts',
+  //   '**/HelloWorld.spec.tsx',
   //   // '**/ObjectId.scalar.spec.ts',
   //   // '**/TypegooseMiddleware.spec.ts',
   // ],
