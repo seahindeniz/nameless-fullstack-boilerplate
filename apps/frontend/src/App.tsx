d958ab9352d8237e45ea2cd4b0e17607fd8a5a08
Sahin Deniz <seahindeniz@gmail.com>
2021-06-23 13:14:52 +0300

    Fix "theme" import and organize

--- apps/frontend/src/App.tsx
+++ apps/frontend/src/App.tsx
@@ -1,9 +1,9 @@
-import { getDarkModeState } from '@components/DarkModeButton/DarkModeToggleButton';
 import { Shell } from '@components';
+import { getDarkModeState } from '@components/DarkModeButton/DarkModeToggleButton';
+import theme from '@utils/theme';
 import { Windmill } from '@windmill/react-ui';
 import { BrowserRouter } from 'react-router-dom';
 import { StyleSheetManager, ThemeProvider } from 'styled-components';
-import { theme } from '@utils';
 import Routes from './routes/routes';
 
 import './assets/main.css';
