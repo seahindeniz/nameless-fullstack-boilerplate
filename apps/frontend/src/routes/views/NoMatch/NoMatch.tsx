83de5afe3392d0bbdebdce2b1a732f0aa910d471
Sahin Deniz <seahindeniz@gmail.com>
2021-06-24 21:36:37 +0300

    Replace tag name

--- apps/frontend/src/routes/views/NoMatch/NoMatch.tsx
+++ apps/frontend/src/routes/views/NoMatch/NoMatch.tsx
@@ -1,7 +1,7 @@
 const NoMatch: React.FC = () => (
   <div className="flex flex-1 flex-column justify-center items-center min-h-full">
     <div className="self-center text-center py-4">
-      <p className="text-4xl">404</p>
+      <h1 className="text-4xl">404</h1>
       <p className="text-2xl text-gray-500">Page not found</p>
     </div>
   </div>
