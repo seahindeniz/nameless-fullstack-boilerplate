6c66dca36e54ee6f239d3bbf1c41dccff05ec03e
Sahin Deniz <seahindeniz@gmail.com>
2021-06-24 21:39:44 +0300

    Refactor class names and remove middle container div

--- apps/frontend/src/routes/views/NoMatch/NoMatch.tsx
+++ apps/frontend/src/routes/views/NoMatch/NoMatch.tsx
@@ -1,9 +1,7 @@
 const NoMatch: React.FC = () => (
-  <div className="flex flex-1 flex-column justify-center items-center min-h-full">
-    <div className="self-center text-center py-4">
-      <h1 className="text-4xl">404</h1>
-      <p className="text-2xl text-gray-500">Page not found</p>
-    </div>
+  <div className="flex flex-col items-center lg:py-4">
+    <h1 className="text-4xl">404</h1>
+    <p className="text-2xl text-gray-500">Page not found</p>
   </div>
 );
 
