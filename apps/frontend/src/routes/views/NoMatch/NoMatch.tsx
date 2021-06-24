bd35804245484a2453965c418e21c64ed5972604
Sahin Deniz <seahindeniz@gmail.com>
2021-06-24 21:31:03 +0300

    Remove extra space

--- apps/frontend/src/routes/views/NoMatch/NoMatch.tsx
+++ apps/frontend/src/routes/views/NoMatch/NoMatch.tsx
@@ -1,5 +1,5 @@
 const NoMatch: React.FC = () => (
-  <div className="flex flex-1 flex-column  justify-center items-center min-h-full">
+  <div className="flex flex-1 flex-column justify-center items-center min-h-full">
     <div className="self-center text-center py-4">
       <p className="text-4xl">404</p>
       <p className="text-2xl text-gray-500">Page not found</p>
